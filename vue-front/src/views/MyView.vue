38d7c33b8bfe94124a8dde59183c2cba967760b8
Suylo <spoownz@yahoo.com>
2023-10-30 11:29:52 +0100

    Remove redundant files from the project
    Deleted files that were not necessary for the functioning of the project . This includes some .idea configuration files, icon files inside /vue-front/src/components/icons folders, and component files HelloWorld.vue and TheWelcome.vue under /vue-front/src/components folders. These files were either auto-generated by the IDE or were part of a template that's not being used in our project, thus removed to keep the codebase clean and maintainable.

---
+++ vue-front/src/views/MyView.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import {Ref, ref} from "vue";
+const amount = ref(0);
+
+type Pizza = {
+  name: string;
+  price: number;
+};
+
+const pizzas: Ref<Pizza[]> = ref([
+  {
+    name: "Margherita",
+    price: 5,
+  },
+  {
+    name: "Pepperoni",
+    price: 7,
+  },
+  {
+    name: "Hawaïenne",
+    price: 8,
+  }
+]);
+
+const pizzaInput = ref("");
+const pizzaPrice = ref("");
+function addPizza(newPizza: string, pizzaPrice: number): void{
+  pizzas.value.push({
+    name: newPizza,
+    price: pizzaPrice,
+  })
+  pizzaInput.value = "";
+}
+</script>
+
+<template>
+  <main>
+    <ul>
+      <li v-for="pizza in pizzas">
+        {{ pizza.name }} - {{ pizza.price }}€
+      </li>
+    </ul>
+
+    <form>
+      <input type="text" name="pizza" id="pizza" v-model="pizzaInput" placeholder="Nom pizza">
+      <input type="number" name="price" id="price" v-model="pizzaPrice" placeholder="Price">
+      <input type="submit" value="Ajouter pizza" @click.prevent="addPizza(pizzaInput, Number(pizzaPrice))">
+    </form>
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  & > div{
+    &:nth-child(1){
+      background: #1e645a;
+    }
+  }
+}
+
+</style>
